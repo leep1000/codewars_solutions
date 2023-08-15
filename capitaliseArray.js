8c4e86075d574e0c16545a0476fcaa3046346337
leep1000 <leepalmerformal@gmail.com>
2023-08-15 04:26:31 +0100

    added map solution

---
+++ capitaliseArray.js
@@ -0,0 +1,8 @@
+// Capitalize each of an array of names
+
+function capitalizeNames(arr) {
+  // your code here
+  return array1.map((x) => x[0].toUpperCase() + x.slice(1));
+}
+
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
